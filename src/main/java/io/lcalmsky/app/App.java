4fef8e1e903ab8e96df85178d4c8f50063fa0ed3
Jaime <lcalmsky@gmail.com>
2021-10-22 05:09:22 +0900

    Update package name

--- src/main/java/io/lcalmsky/app/App.java
+++ src/main/java/io/lcalmsky/app/App.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class SpringBootApp {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootApp.class, args);
+        SpringApplication.run(io.lcalmsky.server.App.class, args);
     }
 
 }
