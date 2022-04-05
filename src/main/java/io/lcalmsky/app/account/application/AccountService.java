9076a310080928e8eebcfcb9fa5a7eb8a995a793
Jaime <lcalmsky@gmail.com>
2022-04-06 01:54:41 +0900

    Abstract email service: #65

--- src/main/java/io/lcalmsky/app/account/application/AccountService.java
+++ src/main/java/io/lcalmsky/app/account/application/AccountService.java
@@ -5,12 +5,13 @@ import io.lcalmsky.app.account.domain.entity.Account;
 import io.lcalmsky.app.account.domain.entity.Zone;
 import io.lcalmsky.app.account.endpoint.controller.SignUpForm;
 import io.lcalmsky.app.account.infra.repository.AccountRepository;
+import io.lcalmsky.app.mail.EmailMessage;
+import io.lcalmsky.app.mail.EmailService;
 import io.lcalmsky.app.settings.controller.NotificationForm;
 import io.lcalmsky.app.settings.controller.Profile;
 import io.lcalmsky.app.tag.domain.entity.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -28,10 +29,11 @@ import java.util.Set;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class AccountService implements UserDetailsService {
 
     private final AccountRepository accountRepository;
-    private final JavaMailSender mailSender;
+    private final EmailService emailService;
     private final PasswordEncoder passwordEncoder;
 
     public Account signUp(SignUpForm signUpForm) {
@@ -47,12 +49,12 @@ public class AccountService implements UserDetailsService {
     }
 
     public void sendVerificationEmail(Account newAccount) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(newAccount.getEmail());
-        mailMessage.setSubject("Webluxible 회원 가입 인증");
-        mailMessage.setText(String.format("/check-email-token?token=%s&email=%s", newAccount.getEmailToken(),
-                newAccount.getEmail()));
-        mailSender.send(mailMessage);
+        emailService.sendEmail(EmailMessage.builder()
+                .to(newAccount.getEmail())
+                .subject("Webluxible 회원 가입 인증")
+                .message(String.format("/check-email-token?token=%s&email=%s", newAccount.getEmailToken(),
+                        newAccount.getEmail()))
+                .build());
     }
 
     public Account findAccountByEmail(String email) {
@@ -104,11 +106,11 @@ public class AccountService implements UserDetailsService {
 
     public void sendLoginLink(Account account) {
         account.generateToken();
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(account.getEmail());
-        mailMessage.setSubject("[Webluxible] 로그인 링크");
-        mailMessage.setText("/login-by-email?token=" + account.getEmailToken() + "&email=" + account.getEmail());
-        mailSender.send(mailMessage);
+        emailService.sendEmail(EmailMessage.builder()
+                .to(account.getEmail())
+                .subject("[Webluxible] 로그인 링크")
+                .message("/login-by-email?token=" + account.getEmailToken() + "&email=" + account.getEmail())
+                .build());
     }
 
     public void addTag(Account account, Tag tag) {
