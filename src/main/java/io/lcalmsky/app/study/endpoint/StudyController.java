927a89d631f4b693b7918f524435df8dadaf8e5d
Jaime (Jungmin Lee) <lcalmsky@gmail.com>
2022-04-12 04:17:18 +0900

    Implement function to create study: resolve #71 (#72)

---
+++ src/main/java/io/lcalmsky/app/study/endpoint/StudyController.java
@@ -0,0 +1,48 @@
+package io.lcalmsky.app.study.endpoint;
+
+import io.lcalmsky.app.account.domain.entity.Account;
+import io.lcalmsky.app.account.support.CurrentUser;
+import io.lcalmsky.app.study.application.StudyService;
+import io.lcalmsky.app.study.domain.entity.Study;
+import io.lcalmsky.app.study.form.StudyForm;
+import io.lcalmsky.app.study.form.validator.StudyFormValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Controller
+@RequiredArgsConstructor
+public class StudyController {
+    private final StudyService studyService;
+    private final StudyFormValidator studyFormValidator;
+
+    @InitBinder("studyForm")
+    public void studyFormInitBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(studyFormValidator);
+    }
+
+    @GetMapping("/new-study")
+    public String newStudyForm(@CurrentUser Account account, Model model) {
+        model.addAttribute(account);
+        model.addAttribute(new StudyForm());
+        return "study/form";
+    }
+
+    @PostMapping("/new-study")
+    public String newStudySubmit(@CurrentUser Account account, @Valid StudyForm studyForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "study/form";
+        }
+        Study newStudy = studyService.createNewStudy(studyForm, account);
+        return "redirect:/study/" + URLEncoder.encode(newStudy.getPath(), StandardCharsets.UTF_8);
+    }
+}
