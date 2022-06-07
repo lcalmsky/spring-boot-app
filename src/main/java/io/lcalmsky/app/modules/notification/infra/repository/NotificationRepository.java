84688775bf088653e741ab7b026e778e8a42ac5f
Jaime <lcalmsky@gmail.com>
2022-06-07 22:49:33 +0900

    Implement web notification: #119

--- src/main/java/io/lcalmsky/app/modules/notification/infra/repository/NotificationRepository.java
+++ src/main/java/io/lcalmsky/app/modules/notification/infra/repository/NotificationRepository.java
@@ -1,7 +1,9 @@
 package io.lcalmsky.app.modules.notification.infra.repository;
 
+import io.lcalmsky.app.modules.account.domain.entity.Account;
 import io.lcalmsky.app.modules.notification.domain.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    long countByAccountAndChecked(Account account, boolean checked);
 }
