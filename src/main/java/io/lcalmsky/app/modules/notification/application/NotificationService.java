4a508ab15177a04a8996659a39c1b0dd0d509932
Jaime <lcalmsky@gmail.com>
2022-06-10 01:43:21 +0900

    Implement list and delete notification: #121

---
+++ src/main/java/io/lcalmsky/app/modules/notification/application/NotificationService.java
@@ -0,0 +1,17 @@
+package io.lcalmsky.app.modules.notification.application;
+
+import io.lcalmsky.app.modules.notification.domain.entity.Notification;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class NotificationService {
+    public void markAsRead(List<Notification> notifications) {
+        notifications.forEach(Notification::read);
+    }
+}
