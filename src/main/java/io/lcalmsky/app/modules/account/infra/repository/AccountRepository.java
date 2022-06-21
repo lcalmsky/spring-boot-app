316856a45783dca56aea2940678c19b87cd2f0ad
Jaime (Jungmin Lee) <lcalmsky@gmail.com>
2022-06-21 23:08:53 +0900

    Implement home: resolve #140 (#143)

--- src/main/java/io/lcalmsky/app/modules/account/infra/repository/AccountRepository.java
+++ src/main/java/io/lcalmsky/app/modules/account/infra/repository/AccountRepository.java
@@ -1,18 +1,23 @@
 package io.lcalmsky.app.modules.account.infra.repository;
 
 import io.lcalmsky.app.modules.account.domain.entity.Account;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
-public interface AccountRepository extends JpaRepository<Account, Long>, QuerydslPredicateExecutor<Account> {
+public interface AccountRepository extends JpaRepository<Account, Long>,
+    QuerydslPredicateExecutor<Account> {
 
-    boolean existsByEmail(String email);
+  boolean existsByEmail(String email);
 
-    boolean existsByNickname(String nickname);
+  boolean existsByNickname(String nickname);
 
-    Account findByEmail(String email);
+  Account findByEmail(String email);
 
-    Account findByNickname(String nickname);
+  Account findByNickname(String nickname);
+
+  @EntityGraph(attributePaths = {"tags", "zones"})
+  Account findAccountWithTagsAndZonesById(Long id);
 }
--- src/main/java/io/lcalmsky/app/modules/main/endpoint/controller/MainController.java
+++ src/main/java/io/lcalmsky/app/modules/main/endpoint/controller/MainController.java
@@ -1,7 +1,9 @@
 package io.lcalmsky.app.modules.main.endpoint.controller;
 
 import io.lcalmsky.app.modules.account.domain.entity.Account;
+import io.lcalmsky.app.modules.account.infra.repository.AccountRepository;
 import io.lcalmsky.app.modules.account.support.CurrentUser;
+import io.lcalmsky.app.modules.event.infra.repository.EnrollmentRepository;
 import io.lcalmsky.app.modules.study.domain.entity.Study;
 import io.lcalmsky.app.modules.study.infra.repository.StudyRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,30 +19,46 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class MainController {
 
-    private final StudyRepository studyRepository;
+  private final StudyRepository studyRepository;
+  private final AccountRepository accountRepository;
+  private final EnrollmentRepository enrollmentRepository;
 
-    @GetMapping("/")
-    public String home(@CurrentUser Account account, Model model) {
-        if (account != null) {
-            model.addAttribute(account);
-        }
-        return "index";
+  @GetMapping("/")
+  public String home(@CurrentUser Account account, Model model) {
+    if (account != null) {
+      Account accountLoaded = accountRepository.findAccountWithTagsAndZonesById(account.getId());
+      model.addAttribute(accountLoaded);
+      model.addAttribute("enrollmentList",
+          enrollmentRepository.findByAccountAndAcceptedOrderByEnrolledAtDesc(accountLoaded, true));
+      model.addAttribute("studyList",
+          studyRepository.findByAccount(accountLoaded.getTags(), accountLoaded.getZones()));
+      model.addAttribute("studyManagerOf",
+          studyRepository.findFirst5ByManagersContainingAndClosedOrderByPublishedDateTimeDesc(
+              account, false));
+      model.addAttribute("studyMemberOf",
+          studyRepository.findFirst5ByMembersContainingAndClosedOrderByPublishedDateTimeDesc(
+              account, false));
+      return "home";
     }
+    model.addAttribute("studyList",
+        studyRepository.findFirst9ByPublishedAndClosedOrderByPublishedDateTimeDesc(true, false));
+    return "index";
+  }
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
+  @GetMapping("/login")
+  public String login() {
+    return "login";
+  }
 
-    @GetMapping("/search/study")
-    public String searchStudy(String keyword, Model model,
-                              @PageableDefault(size = 9, sort = "publishedDateTime", direction = Sort.Direction.ASC) Pageable pageable) {
-        Page<Study> studyPage = studyRepository.findByKeyword(keyword, pageable);
-        model.addAttribute("studyPage", studyPage);
-        model.addAttribute("keyword", keyword);
-        model.addAttribute("sortProperty", pageable.getSort().toString().contains("publishedDateTime")
-                ? "publishedDateTime"
-                : "memberCount");
-        return "search";
-    }
+  @GetMapping("/search/study")
+  public String searchStudy(String keyword, Model model,
+      @PageableDefault(size = 9, sort = "publishedDateTime", direction = Sort.Direction.ASC) Pageable pageable) {
+    Page<Study> studyPage = studyRepository.findByKeyword(keyword, pageable);
+    model.addAttribute("studyPage", studyPage);
+    model.addAttribute("keyword", keyword);
+    model.addAttribute("sortProperty", pageable.getSort().toString().contains("publishedDateTime")
+        ? "publishedDateTime"
+        : "memberCount");
+    return "search";
+  }
 }
